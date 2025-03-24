26346ca4eb097f11f97f259ad6221bb6059cdcf5
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2025-03-23 22:14:09 -0500

    Update section-header.tsx

--- components/ui/section-header.tsx
+++ components/ui/section-header.tsx
@@ -11,14 +11,14 @@ export const SectionHeader: React.FC<SectionHeaderProps> = ({ title, content }) 
       ${content ? "grid" : "flex"} 
       w-full justify-start items-center 
       px-8 py-8 my-2 
-      text-2xl sm:text-3xl lg:text-4xl
+      text-3xl lg:text-4xl
       font-bold text-[#f143a9] 
       border-b-2 border-[#f143a9]
       bg-background backdrop-blur-sm shadow-lg 
       transform transition-transform duration-300
       rounded-t-lg`}>
         {title}
-        {content && <div className="text-[#e0e0ff] md:text-lg flex mt-2 md:mt-0">{content}</div>}
+        {content && <div className="text-[#e0e0ff] text-sm md:text-lg flex mt-2 md:mt-0">{content}</div>}
     </div>
   );
 };
